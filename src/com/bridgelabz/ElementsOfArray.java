e04d98861ea5840028270447af343709b62d2e29
9663461085 <ratansollapure16310@gmail.com>
2022-11-25 12:52:57 +0530

    Added:Printing The elements Of array

---
+++ src/com/bridgelabz/ElementsOfArray.java
@@ -0,0 +1,24 @@
+package com.bridgelabz;
+
+public class ElementsOfArray {
+	public static void main(String[] args) {
+		
+	
+	int array[]=new int[10];
+	array[0]=1;
+	array[1]=2;
+	array[2]=3;
+	array[3]=4;
+	array[4]=5;
+	array[5]=6;
+	array[6]=7;
+	array[7]=8;
+	array[8]=9;
+	array[9]=10;
+	for(int i=0;i<array.length;i++) {
+		System.out.println(array[i]);
+	}
+	
+	
+}
+}
